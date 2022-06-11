2d059484eaf2420b06c67ecbdd29fca25b1afa75
Maran.I.Msabeh <maramismail2015@gmail.com>
2022-06-11 11:11:06 +0300

    setup react app

---
+++ src/components/index.jsx
@@ -0,0 +1,33 @@
+import Button from './Button';
+import Chat from './Chat';
+import ChartsHeader from './ChartsHeader';
+import UserProfile  from  './UserProfile'
+import Navbar  from  './Navbar'
+import Footer  from  './Footer'
+import Header  from  './Header'
+import Notification  from  './Notification'
+import Cart  from  './Cart'
+import Sidebar  from  './Sidebar'
+import ThemeSettings  from  './ThemeSettings'
+import LineChart  from  './Charts/LineChart'
+import Pie  from  './Charts/Pie'
+import Stacked  from  './Charts/Stacked'
+import SparkLine from  './Charts/SparkLine'
+
+export {
+    Button,
+    Chat,
+    ChartsHeader,
+    UserProfile,
+    Navbar,
+    Footer,
+    Header,
+    Notification,
+    Cart,
+    Sidebar,
+    ThemeSettings,
+    LineChart,
+    Pie,
+    Stacked,
+    SparkLine
+};
---
+++ src/pages/index.jsx
@@ -0,0 +1,36 @@
+import Calendar from './Calendar';
+import Ecommerce  from './Ecommerce';
+import Employees from './Employees';
+import Kanban from './Kanban';
+import Editor from './Editor';
+import Customers from './Customers';
+import ColorPicker from './ColorPicker';
+import Orders from './Orders';
+import Area from './Charts/Area';
+import  Bar from './Charts/Bar';
+import LineChart from './Charts/LineChart';
+import Financial from './Charts/Financial';
+import ColorMapping from './Charts/ColorMapping';
+import Stacked from './Charts/Stacked';
+import Pyramid from './Charts/Pyramid';
+import Pie from './Charts/Pie';
+
+export  {
+    Calendar,
+    Ecommerce,
+    Employees,
+    Kanban,
+    Editor,
+    Customers,
+    ColorPicker,
+    Orders,
+    Area,
+    Bar,
+    LineChart,
+    Financial,
+    ColorMapping,
+    Stacked,
+    Pyramid,
+    Pie
+};
+
