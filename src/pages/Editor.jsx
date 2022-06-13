affbdb0626cdc7224b111eedd89c4bdd392c01fe
Maran.I.Msabeh <maramismail2015@gmail.com>
2022-06-14 01:02:39 +0300

    create editor app

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import { Inject,Toolbar, HtmlEditor, Image, Link, QuickToolbar, RichTextEditorComponent } from '@syncfusion/ej2-react-richtexteditor'
+import { Header } from '../components'
+import { EditorData } from '../data/dummy'
 const Editor = () => {
   return (
-    <div>Editor</div>
+    <div className='m-2 md:m-10 p-2 md:p-10 rounded-3xl bg-white'>
+
+      <Header category='App' title='Editor'/>
+
+      <RichTextEditorComponent
+      
+      >
+        <EditorData />
+        <Inject services={[Toolbar,HtmlEditor,Image,Link,QuickToolbar]} />
+      </RichTextEditorComponent>
+    </div>
   )
 }
 
