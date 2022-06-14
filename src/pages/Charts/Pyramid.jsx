a71cda654188255d5a911c58bf8f4abafa89c091
Maran.I.Msabeh <maramismail2015@gmail.com>
2022-06-14 16:16:13 +0300

    create Pyramid chart component

--- src/pages/Charts/Pyramid.jsx
+++ src/pages/Charts/Pyramid.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React from "react";
+import {
+  AccumulationChartComponent,
+  AccumulationSeriesCollectionDirective,
+  AccumulationSeriesDirective,
+  PyramidSeries,
+  Inject,
+  AccumulationLegend,
+  AccumulationDataLabel,
+  AccumulationTooltip,
+} from "@syncfusion/ej2-react-charts";
 
+import { PyramidData } from "../../data/dummy";
+import { useStateContext } from "../../contexts/ContextProvider";
+import { Header } from "../../components";
 const Pyramid = () => {
+  const { currentMode } = useStateContext();
   return (
-    <div>Pyramid</div>
-  )
-}
 
-export default Pyramid
+    <div className="m-4 md:m-10 mt-24  p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+     <Header category="Pyramid" title="Food Comparison Chart"/>
+<div className="w-full">
+<AccumulationChartComponent
+      id="pyramid-chart"
+      tooltip={{ enable: true }}
+      background={currentMode === "Dark" ? "#33373e" : "#fff"}
+      legendSettings={{ background: "white" }}
+    >
+      <AccumulationSeriesCollectionDirective>
+        <AccumulationSeriesDirective
+          dataSource={PyramidData}
+          xName="x"
+          yName="y"
+          type='Pyramid'
+          name="Food"
+          width="45%"
+          height="80%"
+          neckHeight="15%"
+          gapRatio={0.03}
+          explode
+          emptyPointSettings={{ mode: 'Drop', fill: 'red' }}
+          dataLabel={{
+            visible: true,
+            name: "text",
+            position: "Inside",
+    
+          }}
+        ></AccumulationSeriesDirective>
+      </AccumulationSeriesCollectionDirective>
+
+      <Inject
+        services={[
+          AccumulationTooltip,
+          PyramidSeries,
+          AccumulationLegend,
+          AccumulationDataLabel,
+        ]}
+      />
+    </AccumulationChartComponent>
+</div>
+    </div>
+  );
+};
+
+export default Pyramid;
